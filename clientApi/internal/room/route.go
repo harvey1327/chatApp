28b208aa4c4be5a4cecab80739039d7bd0ed5b53
harvey bal <harvey_bal@hotmail.co.uk>
2023-01-13 16:16:04 +0000

    updated messagebroker and model library

--- clientApi/internal/room/route.go
+++ clientApi/internal/room/route.go
@@ -6,33 +6,33 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/harvey1327/chatapp/clientapi/client"
 	"github.com/harvey1327/chatapplib/messagebroker"
-	"github.com/harvey1327/chatapplib/messagebroker/events/createroom"
+	"github.com/harvey1327/chatapplib/models/createroom"
 )
 
-func Route(parent *gin.RouterGroup, publisher messagebroker.Publish, roomClient client.RoomClient) {
+func Route(parent *gin.RouterGroup, publisher messagebroker.Publish[createroom.Model], roomClient client.RoomClient) {
 	r := parent.Group("/room")
 	{
 		r.POST("/create", createRoom(publisher, roomClient))
 	}
 }
 
-func createRoom(publisher messagebroker.Publish, roomClient client.RoomClient) gin.HandlerFunc {
+func createRoom(publisher messagebroker.Publish[createroom.Model], roomClient client.RoomClient) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var request createroom.Model
 		if err := ctx.BindJSON(&request); err != nil {
 			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-		message := messagebroker.PublishMessage(request, createroom.QUEUE_NAME)
-		if err := publisher.Publish(message); err != nil {
+		if message, err := publisher.Publish(request); err != nil {
 			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
+		} else {
+			result, err := roomClient.GetByEventID(message.EventID)
+			if err != nil {
+				ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+			ctx.JSON(200, result)
 		}
-		result, err := roomClient.GetByEventID(message.EventMessage.EventID)
-		if err != nil {
-			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-		ctx.JSON(200, result)
 	}
 }
--- clientApi/internal/user/route.go
+++ clientApi/internal/user/route.go
@@ -6,33 +6,33 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/harvey1327/chatapp/clientapi/client"
 	"github.com/harvey1327/chatapplib/messagebroker"
-	"github.com/harvey1327/chatapplib/messagebroker/events/createuser"
+	"github.com/harvey1327/chatapplib/models/createuser"
 )
 
-func Route(parent *gin.RouterGroup, publisher messagebroker.Publish, userClient client.UserClient) {
+func Route(parent *gin.RouterGroup, publisher messagebroker.Publish[createuser.Model], userClient client.UserClient) {
 	r := parent.Group("/user")
 	{
 		r.POST("/create", createUser(publisher, userClient))
 	}
 }
 
-func createUser(publisher messagebroker.Publish, userClient client.UserClient) gin.HandlerFunc {
+func createUser(publisher messagebroker.Publish[createuser.Model], userClient client.UserClient) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var request createuser.Model
 		if err := ctx.BindJSON(&request); err != nil {
 			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-		message := messagebroker.PublishMessage(request, createuser.QUEUE_NAME)
-		if err := publisher.Publish(message); err != nil {
+		if message, err := publisher.Publish(request); err != nil {
 			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
+		} else {
+			result, err := userClient.GetByEventID(message.EventID)
+			if err != nil {
+				ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+			ctx.JSON(200, result)
 		}
-		result, err := userClient.GetByEventID(message.EventMessage.EventID)
-		if err != nil {
-			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-		ctx.JSON(200, result)
 	}
 }
