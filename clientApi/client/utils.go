2bda7354e8b1b7bf3863921dcc320d8244b42afb
harvey bal <harvey_bal@hotmail.co.uk>
2023-01-17 11:11:42 +0000

    updated retrys

--- clientApi/client/utils.go
+++ clientApi/client/utils.go
@@ -7,17 +7,6 @@ import (
 	"github.com/harvey1327/chatapplib/proto/generated/userpb"
 )
 
-func retry[T any](attempts int, f func(arguments ...interface{}) (T, error)) (res T, err error) {
-	for i := 0; i < attempts; i++ {
-		res, err = f()
-		if err == nil {
-			return res, nil
-		}
-		time.Sleep(10 * time.Millisecond)
-	}
-	return *new(T), err
-}
-
 func retryNonPendingRoom(attempts int, f func(arguments ...interface{}) (*roompb.EventMessage, error)) (res *roompb.EventMessage, err error) {
 	for i := 0; i < attempts; i++ {
 		res, err = f()
--- createusersubscriber/go.mod
+++ createusersubscriber/go.mod
@@ -5,7 +5,7 @@ go 1.19
 require (
 	github.com/harvey1327/chatapplib/database v1.3.0
 	github.com/harvey1327/chatapplib/messagebroker v1.5.0
-	github.com/harvey1327/chatapplib/models v1.3.0
+	github.com/harvey1327/chatapplib/models v1.4.0
 )
 
 require (
